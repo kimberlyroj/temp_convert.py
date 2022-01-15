5a1217c0aae6c57e926e8bcd1331cc691db3911b
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-01-14 21:50:46 -0800

    temp convert

---
+++ temp_convert.py
@@ -0,0 +1,8 @@
+# Author: Kimberly Rojas
+# GitHub username: kimberlyroj
+# Date: 1/13/2021
+# Description: A program that converts Celsius temperatures to Fahrenheit temperatures.
+print("Please enter a Celsius temperature.")
+celsius = float(input())
+Farhenheit = celsius * 9/5 + 32
+print("The equivalent Farhrenheit temperature is", Farhenheit)
